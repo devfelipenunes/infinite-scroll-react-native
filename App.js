cdcd8ba0a318fe5761fd8b6eac5459810c06bfd4
devfelipenunes <devfelipenunes@gmail.com>
2022-09-03 18:48:39 -0300

    Finalizado

--- App.js
+++ App.js
@@ -1,20 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React , {useState, useEffect} from 'react'
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
+
+import axios from 'axios';
 
 export default function App() {
+
+  const baseURL = 'https://api.github.com'
+  const perPage = 20
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [page, setPage] = useState(1)
+
+  useEffect(()=>{
+    loadApi()
+  },[])
+
+  async function loadApi(){
+    if(loading) return
+
+    setLoading(true)
+
+    const response = await axios.get(`${baseURL}/search/repositories?q=react&per_page=${perPage}&page=${page}`)
+
+    setData([...data, ...response.data.items])
+    setPage(page + 1 )
+    setLoading(false)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <FlatList 
+        style={{ marginTop: 35 }}
+        contentContainerStyle={{ marginHorizontal: 20 }}
+        data={data}
+        keyExtractor={item => String(item.id)}
+        renderItem={({item})=> <ListItem data={item} />}
+        onEndReached={loadApi}
+        onEndReachedThreshold={0.1}
+        ListFooterComponent={<FooterList load={loading} />}
+      />
     </View>
   );
+}
+
+function FooterList({load}){
+  if(!load) return null
+  return(
+    <View style={styles.loading}>
+      <ActivityIndicator size={25} color={'#121212'}/>
+    </View>
+  )
+}
+
+function ListItem({data}){
+  return(
+    <View style={styles.listItem}>
+      <Text style={styles.listText}>{data.full_name}</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  listItem:{
+    backgroundColor: '#121212',
+    padding: 30,
+    marginTop: 20,
+    borderRadius:10
+  },
+  listText:{
+    fontSize: 16,
+    color: '#FFF'
+  },
+  loading:{
+    padding: 10
+  }
 });
